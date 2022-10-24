8bb0dd02383d1be5c20811e0bdc66d4e9c255f5d
Your Name <you@example.com>
2022-10-24 13:46:31 +0300

    ADD README

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿while (true)
+{
+    Console.WriteLine("\n\nkelimeleri giriniz: ");
+
+    foreach (var item in Console.ReadLine().Split())
+    {
+        bool def = false;
+        for (int i = 1; i < item.Length; i++)
+        {
+            if (sessizMi(item[i]) == sessizMi(item[i - 1]))
+                def = true;
+        }
+        Console.Write(def + " ");
+    }
+
+    bool sessizMi(char c)
+    {
+        return !"aeıioöuüAEIİOÖUÜ".ToCharArray().Contains(c);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Sessiz-Harf
+Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulaması
